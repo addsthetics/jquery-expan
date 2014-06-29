b22d315e6598751f4ff80b32523b02ebf8245d65
addsthetics <addsthetics@gmail.com>
2014-06-28 22:38:57 -0400

    Set data.

---
+++ team.js
@@ -0,0 +1,110 @@
+team = {
+        "hal" : {
+                    "title"   : "Associate Creative Director",
+                    "fullName" : "",
+                    "bio" : "If you have a question, just ask Hal. The ACD who is part Art Director, part Studio Manager and part amateur race car driver…but not always in that order. ",
+                    "imgSrc" : "images/team/HalNormal.jpg"
+                 },
+        "brian" : {
+                    "title"   : "A/V Art Director",
+                    "fullName" : "Brian Gregory",
+                    "bio" : "Brian Bio went back to original - Change to - He shoots, he edits, and he scores runs on the BigBuzz softball team. When he’s not sitting in the dark of the editing suite, he’s dreaming about winning the Royal Rumble and going on the headline Wrestlemania.",
+                    "imgSrc" : "images/team/BrianNormal.jpg"
+                 },
+        "alexei" : {
+                    "title"   : "Senior Programmer",
+                    "fullName" : "Alexei Roschak",
+                    "bio" : "How do you say “BigBuzz” in Russian? Ask Alexei, the Russian-born programmer. Not only does he create video game characters in his free time, he IS a character.",
+                    "imgSrc" : "images/team/AlexeiNormal.jpg"
+                 },
+         "doug" : {
+                    "title"   : "Co President & CCO",
+                    "fullName" : "Doug Graham",
+                    "bio" : "A diehard sailor and Kevin’s business partner, Doug is probably the only ad man with a degree in Naval Architecture…seriously. Which makes him perfectly suited for keeping BigBuzz afloat (sorry, we couldn’t resist).",
+                    "imgSrc" : "images/team/DougNormal.jpg"
+                 },
+         "kevin" : {
+                    "title"   : "Co President & CCO ",
+                    "fullName" : "Kevin Kelly",
+                    "bio" : "The author of the BigBuzz name, but don’t call it a bee, he hates that. What do you do with a history of being a drummer in a funk rock hair band? You start an ad agency, of course. Kevin is a true tech geek. If he got a tattoo, it would probably be an Apple logo.",
+                    "imgSrc" : "images/team/KevinNormal.jpg"
+         },
+         "liz" : {
+                    "title"   : "Account Executive & Social Media Manager",
+                    "fullName" : "Liz Woodward",
+                    "bio" : "Holds a Masters Degree, but also holds the clients’ hands through the advertising process. When Liz is not managing projects or posting witty Facebook content, she's home chasing her two dogs and posting photos of her culinary creations on Instagram.",
+                    "imgSrc" : "images/team/LizNormal.jpg"
+         },
+         "rachel" : {
+                    "title"   : "Art Director",
+                    "fullName" : "Rachel Chansky",
+                    "bio" : "This Queens native is a talented designer of everything visual.  Even in her free time Rachel keeps busy by designing costumes for Comic Con. Look up in the sky! It’s a bird! It’s a plane! Or it might just be Rachel.",
+                    "imgSrc" : "images/team/RachelNormal.jpg"
+         },
+         "bob" : {
+                    "title"   : "Creative Director",
+                    "fullName" : "Bob Costabile",
+                    "bio" : "His first ad was created when Ronald Reagan was President. He’s a true Madison Avenue Creative veteran with all the stories and awards to prove it. Although, he still says that his favorite advertising job was selling peanuts at Fenway Park when he was a boy.",
+                    "imgSrc" : "images/team/BobNormal.jpg"
+         },
+          "tommy" : {
+                    "title"   : "Director Of Client Services",
+                     "fullName" : "Tommy Spero",
+                    "bio" : "The last thing an agency needs is another drummer, and that’s Tommy. Heading the accounts team is his day job, playing at local bars is what he does after work, which explains why he schedules all those morning meetings. Tommy’s knowledge of all things digital is second only to his knowledge of all things James Brown.",
+                    "imgSrc" : "images/team/TommyNormal.jpg"
+         },
+         "kim" : {
+                    "title"   : "Traffic Manager",
+                    "fullName" : "Kim Lewis",
+                    "bio" : "Do you know the difference between a slash and a biff? Kim, AKA “Shred Betty” does. This snowboarding traffic manager is the only Buzzer that prays for snow. No doubt that her amazing coordination comes from her history of dancing with the Russian ballet as a child.",
+                    "imgSrc" : "images/team/KimNormal.jpg"
+         },
+          "keasean" : {
+                    "title"   : "Programmer",
+                    "fullName" : "Keasean Souffrant",
+                    "bio" : "Programmer and BigBuzz fashion icon. When he’s not staring at a computer screen for hours coding for a client, he’s home…staring at a computer screen for hours designing games.",
+                    "imgSrc" : "images/team/KeaseanNormal.jpg"
+         },
+         "mary" : {
+                    "title"   : "Director Of Finance & Administration",
+                    "fullName" : "Mary Stanley",
+                    "bio" : "Once dreamed of becoming a New York City cop. Mary is the person you want to make friends with at BigBuzz, because she writes the checks. When Mary is not balancing the books, she’s balancing a career and a family of three children and a husband.",
+                     "imgSrc" : "images/team/MaryNormal.jpg"
+         },
+          "alex" : {
+                    "title"   : "Head Of Programming",
+                    "fullName" : "Alex Lee",
+                    "bio" : "This Head of Programming holds the YouTube view record for “Gangnam Style.” He’s also a lover of anything Apple (the company, not the fruit). Alex hails from a part of the USSR currently called Tashkent, Uzbekistan.",
+                     "imgSrc" : "images/team/AlexNormal.jpg"
+         },
+         "joel" : {
+                    "title"   : "Programmer",
+                    "fullName" : "Joel Paulino",
+                    "bio" : "This programming wiz just graduated from MIT with a free ride. If you think that’s impressive, he can also finish a Rubik’s cube in under 30 seconds. Joel lists his favorite movie as “Who Framed Roger Rabbit’,” but you would think it would be, “A Beautiful Mind.”",
+                     "imgSrc" : "images/team/JoelNormal.jpg"
+         },
+          "negeen" : {
+                    "title"   : "Digital Engagement Operative",
+                    "fullName" : "Negeen Ghaisar",
+                    "bio" : "This Digital Engagement Operative was born and raised in McLean, Virginia, also the home of the CIA headquarters. Negeen is fluent in Farsi (Persian), which is almost as unique as her cat’s name, Jean Paul Wolverine.",
+                     "imgSrc" : "images/team/NegeenNormal.jpg"
+         },
+         "laura" : {
+                    "title"   : "Intern",
+                    "fullName" : "Laura Casciola",
+                    "bio" : "Uberintern. Currently a junior at Syracuse, Laura is looking forward to travelling through Europe this spring. She’s already got a jump on the trip because she is fluent in Italian. Ciao!",
+                    "imgSrc" : "images/team/LauraNormal.jpg"
+         },
+         "brianS" : {
+                    "title"   : "Digital Engagement Manager",
+                    "fullName" : "Brian Seskin",
+                    "bio" : "",
+                    "imgSrc" : "images/team/BrainSNormal.jpg"
+         },
+         "cesar" : {
+                    "title"   : "Art Director   ",
+                    "fullName" : "Cesar Maldonado",
+                    "bio" : "Our resident hipster designer, Cesar can also be seen behind two turntables with a microphone in hand.This buzzer once dreamed of being an archeologist after seeing Indiana Jones, but opted for a safer position at BigBuzz. No rolling boulders here.",
+                    "imgSrc" : "images/team/CesarNormal.jpg"
+         }
+};
